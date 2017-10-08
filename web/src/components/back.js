18989145bc74309e9ba92954b474021748a5a76c
gillesgerlinger <gilles.gerlinger@nokia.com>
2017-10-08 13:10:47 +0200

    analytics

--- web/src/components/back.js
+++ web/src/components/back.js
@@ -3,12 +3,19 @@
   Copyright Nokia 2017. All rights reserved.
 */
 
+import {Config} from './../config.js';
+
 class B {
   back = false;
   set(component) {
     this.component = component;
     this.history = component.props.history;
     this.pathname = component.props.location.pathname;
+    if (window.gtag && this.currentPath !== window.location.pathname) {
+      this.currentPath = window.location.pathname;
+      window.gtag('config', Config.trackingID);
+      // console.log('ga');
+    }    
   }
 }
 
