cb37877e327734e7206071e3c0766d569f35fc78
lambeth <thomas.lambeir@phpro.be>
2019-01-09 11:25:18 +0100

    removed comments from python script

--- web/public/cron_recommendations.py
+++ web/public/cron_recommendations.py
@@ -13,7 +13,6 @@ def strip_tags(html):
     
 class MLStripper(HTMLParser):
     def __init__(self):
-        #super().__init__()
         self.reset()
         self.strict = False
         self.convert_charrefs= True
@@ -27,11 +26,8 @@ def cleanDataFrame(ds, useDescription, useTitle, useKeywords):
     indexesToBeRemoved = []
     for idx, row in ds.iterrows():
 
-        # remove short descriptions
         if len( str(row['Description']).split()) < 10:
           indexesToBeRemoved.append(idx);
-
-        # remove html tags
         description = ""
         if useDescription == 'on' and 'Description' in ds.columns:
             description += str(row['Description'])
@@ -40,27 +36,19 @@ def cleanDataFrame(ds, useDescription, useTitle, useKeywords):
         if useKeywords == 'on' and 'Keywords' in ds.columns:
             description += str(row['Keywords'])
         description = strip_tags(description)
-        # description = strip_tags(row['Description'])
-        # remove whitespace and punctuation
         words = description.split()
         words = [word.lower() for word in words]
         table = str.maketrans('', '', string.punctuation)
         description = [w.translate(table) for w in words]
         description = ' '.join(map(str, description))
-        
-        # remove stop words
         stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
         for stop_word in stop_words:
             description = description.replace(' '+stop_word+' ', ' ')
-        
         ds.set_value(idx,'Description',description)
-
-        # remove collections
         if not isinstance(row['Solutions'], float):
             length = len(row['Solutions'])
             if length > 0:
               indexesToBeRemoved.append(idx);
-
     ds = ds.drop(indexesToBeRemoved)
     ds = ds.reset_index()
     return ds;
@@ -70,7 +58,6 @@ class Recommendation(object):
     title = ""
     score = 0
 
-    # The class "constructor" - It's actually an initializer
     def __init__(self, index, title, score):
         self.index = index
         self.title = title
@@ -89,12 +76,11 @@ def return_recommendations_for_id(itemid):
             if rec[0] > 0.3:
                 recommendations.append(rec[1])
                 i += 1
-        # Do something
     except KeyError:
         pass
     return recommendations;
 
-ds = pd.read_json('employee.json') #you can plug in your own list of products or movies or books here as csv file
+ds = pd.read_json('employee.json')
 ds = cleanDataFrame(ds, 'on', 'on', 'off');
 
 
@@ -103,11 +89,10 @@ tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=3, stop_words='
 tfidf_matrix = tf.fit_transform(ds['Description'].astype(str))
 cosine_similarities = cosine_similarity(tfidf_matrix,tfidf_matrix)
 
-results = {} # dictionary created to store the result in a dictionary format (ID : (Score,item_id))
+results = {}
 
-for idx, row in ds.iterrows(): #iterates through all the rows
-    # the below code 'similar_indice' stores similar ids based on cosine similarity. sorts them in ascending order. [:-5:-1] is then used so that the indices with most similarity are got. 0 means no similarity and 1 means perfect similarity
-    similar_indices = cosine_similarities[idx].argsort()[:-12:-1] #stores 10 most similar books, you can change it as per your needs
+for idx, row in ds.iterrows():
+    similar_indices = cosine_similarities[idx].argsort()[:-12:-1] 
     similar_items = [(cosine_similarities[idx][i], ds['ID'][i]) for i in similar_indices]
     results[row['ID']] = similar_items[1:]
 
